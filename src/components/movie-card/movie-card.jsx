ec4ffa3e7682254339af814b775905ab4846a25f
andrasbanki <abankiandras@gmail.com>
2021-07-05 18:36:05 +0200

    Added remove and add favorite buttons

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,25 +2,60 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import { Container } from 'react-bootstrap';
+import axios from 'axios';
 import { Link } from "react-router-dom";
 
 export class MovieCard extends React.Component {
-  render() {
-    const { movie } = this.props;
 
-    return (
-      <Card border="dark" style={{ width: '15 rem', height: '62rem', margin: '5 rem' }} >
+  handleAdd() {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+    axios.post(`https://andrasbanki-myflixapp.herokuapp.com/users/${user}` + "/favorites/" +
+      this.props.movie._id, {},
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+      .then((response) => {
+        console.log(response);
+        alert(this.props.movie.Title + " has been added to your favorites!");
+      })
+  }
+
+  handleRemove() {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+    axios.delete(`https://andrasbanki-myflixapp.herokuapp.com/users/${user}` + "/favorites/" +
+      this.props.movie._id, {},
+      { headers: { Authorization: `Bearer ${token}` } }
+    )
+      .then((response) => {
+        console.log(response);
+        alert(this.props.movie.Title + " has been removed from your favorites!");
+      })
+  }
+  
+  render() {
+    const { movie, } = this.props;
+    
+  return (
+    <Container>
+      <Card border="dark" style={{ width: '12rem', height: '80rem' }} >
         <Card.Img variant="top" rounded="true" src={movie.imageUrl} />
         <Card.Body>
           <Card.Title style={{fontSize: 28}}>{movie.Title}</Card.Title>
-
           <Card.Text>{movie.Description}</Card.Text>
           <Link to={`/movies/${movie._id}`}>
             <Button variant="outline-secondary">See More</Button>
           </Link>
-          </Card.Body>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="outline-success" onClick={() => this.handleAdd(movie)}>Add to favorites</Button>
+          </Link>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="outline-danger" onClick={() => this.handleRemove(movie)}>Remove from favorites</Button>
+          </Link>
+        </Card.Body>
       </Card>
+      </Container>
     );
   }
 }
